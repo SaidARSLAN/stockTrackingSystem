6449dc9a42a3a47c9518608a468e1ae09154990d
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-01 13:58:46 +0300

    Added navbar, hero sections and gave some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
+import Nav from './components/Nav'
+import Hero from './sections/Hero'
 
 function App() {
 
   return (
-    <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className='w-full h-full bg-black'>
+      <Nav />
+      <section>
+        <Hero />
+      </section>
+    </main>
   )
 }
 
