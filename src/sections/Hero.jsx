ce63990f85dfa8ec4173a886992475e8a41546b8
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-01 15:18:19 +0300

    Added hero basic sections

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='w-full h-full flex flex-col items-center justify-center'>
+                <h3 className='text-white text-sm font-roboto text-center'>Creating your financial future from zero</h3>
+                <h1 className='text-white text-5xl text-center'>Digital Financial Wallet With Tracking System</h1>
+                <p className='text-gray-500 text-sm text-center'>Create your own wallet and track your best values in so many countries stock markets, and then learn basic financial skills and use them. This is more than wallet and tracking system!</p>
+    </div>
   )
 }
 
