228b59b61755eb82d8029090fe603d8020885e59
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-02 23:54:04 +0300

    fixed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='w-full h-full flex flex-col lg:flex-row items-center justify-center space-y-2'>
         <div className='w-full h-full flex flex-col items-center justify-center space-y-4 lg:items-start lg:px-12'>
-                <h3 className='text-white text-sm font-roboto text-center tracking-wide cursor-default italic lg:text-base lg:text-left'>Forging Your Financial Future from Ground Zero</h3>
+                <h3 className='text-white text-sm font-roboto text-center tracking-wide cursor-default lg:px-3 italic lg:text-base lg:text-left'>Forging Your Financial Future from Ground Zero</h3>
                 <h1 className='text-white text-5xl text-center font-roboto leading-relaxed -tracking-wide cursor-default lg:text-left lg:text-7xl px-2'>Digital Wallet Integrated Tracking System</h1>
                 <p className='text-gray-500 text-sm text-center font-roboto px-12 leading-relaxed tracking-wide cursor-default lg:text-left lg:px-0 lg:text-base'>Create your own wallet and track your best values in so many countries stock markets, and then learn basic financial skills and use them. This is more than wallet and tracking system!</p>
                 <div className='w-full flex items-center justify-center lg:justify-start'>
