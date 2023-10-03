9112eddc6589e244b026ffdfdd00a03c7b5b2f24
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-03 14:50:07 +0300

    Removed topfivesection

--- src/sections/TopFiveCompanies.jsx
+++ src/sections/TopFiveCompanies.jsx
@@ -1,27 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react'
 import GlobalContext from '../context/MainContext'
 
 const TopFiveCompanies = () => {
+    const [currCompany, setCurrCompany] = useState("");
+
   return (
-    <div className='px-12'>
-        <div className='flex w-full items-center justify-between'>
-        <div className='flex items-center justify-center bg-gray-700 px-4 py-2 hover:bg-gray-900 cursor-pointer rounded-md'>
-            <h3 className='text-xl text-white font-roboto font-bold tracking-widest'>APPLE</h3>
-        </div>
-        <div className=''>
-            <h3>APPLE</h3>
-        </div>
-        <div className=''>
-            <h3>APPLE</h3>
-        </div>
-        <div className=''>
-            <h3>APPLE</h3>
-        </div>
-        <div className=''>
-            <h3>APPLE</h3>
-        </div>
-        </div>
+    <div>
+        
     </div>
   )
 }
