bff1bb75be918bc0584449814b7d94b9bc23c25c
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-05 20:48:59 +0300

    Added sign in page animations

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import signup from '../assets/signup.png'
 import { useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 const userInfo = {
     "username" : "admin",
@@ -22,18 +23,18 @@ const SignUp = () => {
 
   return (
     <div className='flex flex-col  justify-center space-y-4 my-4 lg:px-12 px-4'>
-      <h1 className='text-3xl  lg:text-6xl my-4 text-center lg:text-left font-roboto font-bold text-white tracking-widest lg:px-12'>Sign In</h1>
+      <motion.h1 className='text-3xl  lg:text-6xl my-4 text-center lg:text-left font-roboto font-bold text-white tracking-widest lg:px-12' initial={{y:"-100vw"}} animate={{y:"0"}}>Sign In</motion.h1>
     <div className='flex items-center  justify-center w-full'>
-      <div className=' flex items-center justify-center w-2/3'>
+      <motion.div className=' flex items-center justify-center w-2/3' initial={{x:"-100vw"}} animate={{x:0}}>
         <img src={signup}/>
-      </div>
-        <form className='w-full h-full flex items-center justify-start flex-col space-y-4'>
+      </motion.div>
+        <motion.form initial={{x:"100vw"}} animate={{x:0}} transition={{type:'spring', stiffness:120}} className='w-full h-full flex items-center justify-start flex-col space-y-4'>
               <input className='w-[480px] px-4 py-4 border-2 border-gray-600 duration-200 hover:border-white customBG text-white' value={username} onChange={e => setUsername(e.target.value)} placeholder='Username'/>
               <input type='password' className='w-[480px] px-4 py-4 border-2 border-gray-600 duration-200 hover:border-white customBG text-white' value={password} onChange={e => setPassword(e.target.value)}  placeholder='Password'/>
               <div className='w-[480px] flex items-center justify-center lg:justify-start'>
               <button className='w-[200px] customBG px-4 py-2 text-gray-400 font-bold font-roboto tracking-widest hover:text-white duration-200 text-lg' onClick={handleClick}>Login</button>
               </div>
-        </form>
+        </motion.form>
     </div>
     </div>
   )
