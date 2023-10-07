ec12a45489a9fa471310cb948062801cfd347e09
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-07 17:57:02 +0300

    Added profile section items

--- src/components/Mail.jsx
+++ src/components/Mail.jsx
@@ -22,7 +22,7 @@ const Mail = () => {
       
   return (
     <motion.form className='flex items-center justify-center space-x-4 w-full' initial={{x:"100vw"}} animate={{x:0}} transition={{type:"tween", duration:0.4}}>
-                <input className='text-black px-4 py-2 font-roboto tracking-wide w-[250px]' value={email} onChange={event => {setEmail(event.target.value)}} placeholder='Mail Adress'></input>
+                <input className='text-black px-4 py-2 font-roboto tracking-wide w-[180px] lg:w-[320px]' value={email} onChange={event => {setEmail(event.target.value)}} placeholder='Mail Adress'></input>
                 <button className='tableBackground text-white px-4 py-2 font-roboto font-bold tracking-wide' onClick={handleClick}>Create</button>
             </motion.form>
   )
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -6,9 +6,11 @@ const Wallet = () => {
     const {data} = useContext(GlobalInformationContext)
 
   return (
-    <div className='w-full h-screen'>
-            <h1 className='text-white'>{data.nameSurname}</h1>
-            <h1 className='text-white'>{data.email}</h1>
+    <div className='w-full h-screen px-12'>
+        <div className='w-full border-2 border-gray-500 px-4 py-4'>
+            <h1 className='text-white font-roboto font-bold text-2xl tracking-wide'>{data.nameSurname}</h1>
+            <h1 className='text-white font-roboto  text-sm tracking-wider'>{data.email}</h1>
+            </div>
     </div>
   )
 }
