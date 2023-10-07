ef1a01df99ab2f81d7c3dadff5e60614d68755b1
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-07 17:45:54 +0300

    fixed input

--- src/components/NameAndSurname.jsx
+++ src/components/NameAndSurname.jsx
@@ -18,7 +18,7 @@ const NameAndSurname = () => {
 
   return (
     <motion.form className='flex items-center justify-center space-x-4 w-full' initial={{x: "100vw"}} animate={{x:0}} transition={{type:"tween", duration:0.4}}>
-                <input className='text-black px-4 py-2 font-roboto tracking-wide w-[280px]' value={nameSurname} onChange={event => setNameSurname(event.target.value)} placeholder='Your name And Surname'></input>
+                <input className='text-black px-4 py-2 font-roboto tracking-wide w-[250px]' value={nameSurname} onChange={event => setNameSurname(event.target.value)} placeholder='Your name And Surname'></input>
                 <button className='tableBackground text-white px-4 py-2 font-roboto font-bold tracking-wide' onClick={handleClick}>Continue</button>
             </motion.form>
   )
