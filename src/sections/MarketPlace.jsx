59810ab7e1b3f9f3419a2a360a5296e913a7d60c
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-07 11:51:25 +0300

    Added marketplace section

--- src/sections/MarketPlace.jsx
+++ src/sections/MarketPlace.jsx
@@ -4,25 +4,31 @@ import GlobalContext from '../context/MainContext'
 const MarketPlace = () => {
     const {currCompanyInfos} = useContext(GlobalContext)
   return (
-    <section id='market' className='grid grid-cols-1 w-full px-12 gap-y-4 py-20 lg:py-0'>
+    <section id='market' className='grid grid-cols-1 w-full px-12 py-20 lg:py-0'>
         <div className='w-full'>
             <h1 className='text-white font-roboto text-3xl font-bold text-center lg:text-left lg:text-5xl my-4'>Market Update</h1>
         </div>
         <div className='flex w-full items-center justify-between  px-4 lg:px-12  tableBackground py-4'>
+          <div className='w-full'>
                 <h1 className='text-white font-roboto font-bold text-sm lg:text-xl'>Company Name</h1>
+                </div>
+                <div className='w-full max-sm:hidden'>
                 <p className='text-white font-roboto font-bold text-sm lg:text-xl'>Industry</p>
+                </div>
+                <div className=''>
                 <p className='text-white font-roboto font-bold text-sm lg:text-xl'>Price</p>
+                </div>
         </div>
         {currCompanyInfos.map((company) => {
-            return <div className='w-[600px]  lg:w-full px-4 lg:px-12 justify-between items-center flex  h-16 cursor-pointer'>
-              <div className='w-[50px] lg:w-full lg:flex items-center justify-start'>
-            <h4 className='font-roboto text-sm lg:text-lg text-white'>{company[0].companyName}</h4>
+            return <div className='w-full lg:w-full px-4 lg:px-12 justify-between items-center flex  h-16 cursor-pointer hover:bg-[rgba(41,35,103,0.3)] duration-200'>
+              <div className='text-left w-full'>
+            <h4 className='font-roboto text-xs lg:text-xl lg:font-bold text-white '>{company[0].companyName}</h4>
             </div>
-            <div className='w-[100px] lg:w-full lg:flex items-center lg:mr-24 justify-end'>
-            <p className='font-roboto text-sm text-white w-40'>{company[0].industry}</p>
+            <div className='w-full max-sm:hidden'>
+            <p className='font-roboto text-xs text-white lg:text-lg lg:whitespace-nowrap w-40'>{company[0].industry}</p>
             </div>
-            <div className='w-[270px] lg:w-full lg:flex items-center justify-end'>
-            <p className='font-roboto lg:text-lg text-green-400 text-sm'>{company[0].price} $</p>
+            <div className=''>
+            <p className='font-roboto lg:text-lg text-green-400 text-xs  whitespace-nowrap'>{company[0].price} $</p>
             </div>
     </div>
         })}
