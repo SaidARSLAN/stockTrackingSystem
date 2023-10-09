b33e2d0135f3b96dcf6d6686ebbc6a014b6dedbc
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-09 12:46:15 +0300

    Added modal

--- src/components/ExpenseTrackerSystem.jsx
+++ src/components/ExpenseTrackerSystem.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import GlobalInformationContext from '../context/InformationContext'
-
+import Modal from 'react-modal';
 const ExpenseTrackerSystem = () => {
     const {income} = useContext(GlobalInformationContext)
     const [transaction, setTransaction] = useState("");
@@ -15,6 +15,19 @@ const ExpenseTrackerSystem = () => {
         setTransaction("");
         setPrice("");
     }
+
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: "#fff",
+        },
+      };
+
   return (
     <div className='w-full flex flex-col items-center justify-center'>
     <div className='w-full flex items-center justify-evenly'>
@@ -32,19 +45,25 @@ const ExpenseTrackerSystem = () => {
                 return <div className='flex w-full items-center justify-between border-2 px-2 py-1'><h3 className='text-white font-roboto font-bold'>{query.name}</h3><p className='text-rose-500 max-[320px]:whitespace-nowrap'>{query.price} $</p></div>
             })}
         </div>
-        {toggle ? <form className='w-full flex flex-col items-center justify-center my-4 space-x-2 relative'>
-            <div className='flex w-full justify-end items-center mr-40 my-2 max-[320px]:mr-10'>
-                <h3 className='text-white text-3xl font-bold cursor-pointer' onClick={() => {setToggle(false)}}>X</h3>
+        <button className='bg-white px-2 py-1 my-6 font-roboto font-bold' onClick={() => {setToggle(true)}}>Create Transaction</button>
+        <Modal
+        isOpen={toggle}
+        contentLabel="Example Modal"
+        style={customStyles}
+      >
+        <div className='flex w-full justify-end items-center my-2 max-[320px]:mr-10'>
+                <h3 className='text-black text-3xl font-bold cursor-pointer' onClick={() => {setToggle(false)}}>X</h3>
             </div>
+        <form className='w-full flex flex-col items-center justify-center my-4 space-x-2 relative'>
             <div className='flex flex-col items-center justify-center space-y-2'>
-            <input placeholder='Transaction' className='px-2 py-2 font-roboto font-bold tracking-wide max-[320px]:w-[150px]' value={transaction} onChange={event => setTransaction(event.target.value)}></input>
-            <input placeholder='Price' className={price  === 0 ? 'px-2 py-2 font-roboto text-gray-400 font-bold tracking-wide max-[320px]:w-[150px]'   : 'px-2 py-2 font-roboto font-bold tracking-wide max-[320px]:w-[150px]'} value={price} onChange={event => setPrice(event.target.value)}></input>
-            <button className='w-full py-1 bg-white font-roboto font-bold' onClick={handleClick}>Add</button>
+            <input placeholder='Transaction' className='px-2 py-2 border-2 border-black font-roboto font-bold tracking-wide max-[320px]:w-[150px]' value={transaction} onChange={event => setTransaction(event.target.value)}></input>
+            <input placeholder='Price' className={price  === 0 ? 'px-2 py-2 border-black border-2 font-roboto text-gray-400 font-bold tracking-wide max-[320px]:w-[150px]'   : 'px-2 py-2 border-black border-2 font-roboto font-bold tracking-wide max-[320px]:w-[150px]'} value={price} onChange={event => setPrice(event.target.value)}></input>
+            <button className='w-full py-1 bg-gray-900 text-white font-roboto font-bold ' onClick={handleClick}>Add</button>
             </div>
-        </form> : <button className='bg-white px-2 py-1 my-6 font-roboto font-bold' onClick={() => {setToggle(true)}}>Create Transaction</button>}
-        
+        </form>
+      </Modal>
     </div>
-  )
+  ) 
 }
 
 export default ExpenseTrackerSystem
