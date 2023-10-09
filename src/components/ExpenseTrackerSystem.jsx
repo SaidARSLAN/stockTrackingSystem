102d028472d565eb28053c1fc6bb43426fc6d861
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-09 12:14:45 +0300

    Added total Count

--- src/components/ExpenseTrackerSystem.jsx
+++ src/components/ExpenseTrackerSystem.jsx
@@ -1,19 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import GlobalInformationContext from '../context/InformationContext'
 
 const ExpenseTrackerSystem = () => {
-    const {income,sendTransactionData} = useContext(GlobalInformationContext)
+    const {income} = useContext(GlobalInformationContext)
     const [transaction, setTransaction] = useState("");
     const [price, setPrice] = useState("");
     const [transactionList, settransactionList] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
     const [toggle, setToggle] = useState(false);
     const handleClick = (event) => {
         event.preventDefault();
         settransactionList([...transactionList, {"name" : transaction,"price" : price}])
+        setTotalPrice(prevTotalCount => prevTotalCount + parseInt(price));
         setTransaction("");
         setPrice("");
     }
-
   return (
     <div className='w-full flex flex-col items-center justify-center'>
     <div className='w-full flex items-center justify-evenly'>
@@ -23,10 +24,10 @@ const ExpenseTrackerSystem = () => {
         </div>
         <div className='flex items-center flex-col justify-center space-y-2'>
         <h3 className='text-white text-xl tracking-wider font-roboto font-bold'>Expense</h3>
-        <h3 className='text-rose-500 text-lg font-roboto font-bold'>-200 $</h3>
+        <h3 className='text-rose-500 text-lg font-roboto font-bold'>{totalPrice} $</h3>
         </div>
         </div>
-        <div className='flex items-center justify-center flex-col w-full px-16'>
+        <div className='flex items-center justify-center flex-col w-full px-16 space-y-4 py-4'>
             {transactionList.map((query) => {
                 return <div className='flex w-full items-center justify-between border-2 px-2 py-1'><h3 className='text-white font-roboto font-bold'>{query.name}</h3><p className='text-rose-500'>{query.price} $</p></div>
             })}
