5d4cb7da1db24d3cd997bbfc94581f0dfea78d8d
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-16 21:09:06 +0300

    Added form control

--- src/components/NameAndSurname.jsx
+++ src/components/NameAndSurname.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import GlobalContext from '../context/MainContext'
 import { motion, steps } from 'framer-motion';
 import GlobalInformationContext from '../context/InformationContext';
@@ -9,16 +9,21 @@ const NameAndSurname = () => {
     const [nameSurname, setNameSurname] = useState("");
     const {setStep} = useContext(GlobalContext);
     const {sendData} = useContext(GlobalInformationContext);
-
+    const inputRef = useRef();
     const handleClick = (event) => {
-          event.preventDefault();
-          setStep(step => step + 1);
-          sendData(nameSurname,"name")
+      event.preventDefault()
+          nameSurname !== "" ? 
+          (
+          setStep(step => step + 1),
+          sendData(nameSurname,"name"))
+          :
+          inputRef.current.classList.add("border-2")
+          inputRef.current.classList.add("border-rose-500");
     }
 
   return (
     <motion.form className='flex items-center justify-center space-x-4 w-full' initial={{x: "100vw"}} animate={{x:0}} transition={{type:"tween", duration:0.4}}>
-                <input className='text-black px-4 py-2 font-roboto tracking-wide w-[180px] lg:w-[320px]' value={nameSurname} onChange={event => setNameSurname(event.target.value)} placeholder='Your name And Surname'></input>
+                <input ref={inputRef} className='text-black px-4 py-2 font-roboto tracking-wide w-[180px] lg:w-[320px]' value={nameSurname} onChange={event => setNameSurname(event.target.value)} placeholder='Your name And Surname'></input>
                 <button className='tableBackground text-white px-4 py-2 font-roboto font-bold tracking-wide' onClick={handleClick}>Continue</button>
             </motion.form>
   )
