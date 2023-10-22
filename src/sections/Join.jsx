c653ed0002ec7f7dac8b8cdc46c11bec1a6299f7
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-22 23:27:00 +0300

    Fixed styles

--- src/sections/Join.jsx
+++ src/sections/Join.jsx
@@ -4,7 +4,7 @@ const Join = () => {
   return (
     <div className='w-full h-full flex items-center justify-start flex-col py-24'>
         <h1 className='text-white text-6xl font-roboto text-center lg:text-8xl px-24 lg:px-0 font-extrabold leading-tight tracking-wider'>CREATE YOUR OWN<span className='block tableBackground bg-clip-text text-transparent'>WALLET</span></h1>
-        <p className='text-white font-roboto  text-xl lg:px-0 px-32 text-center lg:text-xl font-semibold tracking-wide my-4'>Invest and manage all your invests at one place</p>
+        <p className='text-white font-roboto  text-xl lg:px-0 px-20 text-center lg:text-xl font-semibold tracking-wide my-4'>Invest and manage all your invests at one place</p>
         <button className='text-white font-roboto mt-12  tracking-widest rounded-xl tableBackground px-6 py-4  text-xl font-bold'>Start with Stking</button>
     </div>
   )
