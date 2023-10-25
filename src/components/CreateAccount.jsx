2283ccd89d0da38b530c2bce792718ac477e1895
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-25 13:27:40 +0300

    Fixed register section

--- src/components/CreateAccount.jsx
+++ src/components/CreateAccount.jsx
@@ -1,8 +1,11 @@
+import { Formik } from 'formik';
 import React, { useEffect, useState } from 'react'
 import ClipLoader from 'react-spinners/ClipLoader'
+
+
 const CreateAccount = () => {
     const [open, setOpen] = useState(true);
-
+    
     useEffect(() => {
         setTimeout(() => {
                 setOpen(false)
@@ -11,32 +14,17 @@ const CreateAccount = () => {
 
   return (
     <>        
-    {open ? <ClipLoader color={'white'} size={50}/> : 
-        <form className='flex flex-col space-y-4 my-12'>        
-        <div className='flex flex-col'>
-        <h1 className='text-white font-roboto text-2xl mb-4 font-bold cursor-default'>Personal Information</h1>
-        <div className='flex space-x-12 mb-4'>
-            <input type="text" className='heroBackground border-[1px] text-white px-4 py-1' placeholder='First Name' />
-            <input type="text" className='heroBackground border-[1px] text-white px-4 py-1' placeholder='Last Name' />
-        </div>
-        <div>
-        <input type="text" className='heroBackground border-[1px] w-full mb-4 text-white px-4 py-1' placeholder='Email' />
-        </div>
-        <div className='w-full flex items-center justify-evenly my-4'>
-            <div className='flex flex-col space-y-1'>
-            <input type="radio"/>
-            <label className='text-white font-roboto'>Male</label>
-            </div>
-            <div className='flex flex-col space-y-1'>
-            <input type="radio"/>
-            <label className='text-white font-roboto'>Female</label>
-            </div>
-        </div>
-        </div>
-        <div className='flex flex-col'>
-        <h1 className='text-white font-roboto text-2xl mb-4 font-bold cursor-default'>Financial Information</h1>
-        </div> 
-        </form>
+    {
+    open 
+    ? 
+    <ClipLoader color={'white'} size={50}/> 
+    : 
+    
+    <Formik initialValues={{firstname:'',lastname:'',email:'',password:'',confirm:''}}
+     className='flex flex-col space-y-4 my-8'>        
+        <h1 className='text-white font-bold font-roboto text-4xl tracking-wide cursor-default'>Subscribe</h1>
+        
+    </Formik>
 }
 </>
 
