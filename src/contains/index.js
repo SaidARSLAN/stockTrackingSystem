732f969b1c743cf49a59b434b99b4a94dc61d8ea
SaidARSLAN <lost.myreligion@hotmail.com>
2023-10-25 14:46:07 +0300

    Fixd navba

--- src/contains/index.js
+++ src/contains/index.js
@@ -9,7 +9,7 @@ export const navLinks = [
     {"name":"Home","link":"#home"},
     {"name":"Market","link":"#market"},
     {"name":"About","link":"#about"},
-    {"name":"Wallet","link":"#wallet"}
+    {"name":"join","link":"#join"}
 ]
 
 
